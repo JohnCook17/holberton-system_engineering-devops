1a04c1acc28513255f1d0acc6cd2e0642fe6832b
JohnCook17 <655@holbertonschool.com>
2019-08-08 14:15:47 -0700

    changed the order of operations

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,15 +1,17 @@
-package {'nginx':
+package {'Install nginx':
     ensure => installed,
-}
-file {'path':
-    content => 'Holberton School',
-    path    => '/var/www/html/index.html',
+    name => 'nginx',
 }
 file_line {'title':
     ensure => 'present',
     path   => '/etc/nginx/sites-available/default',
     after  => 'listen 80 default_server;',
     line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+    multiple => true,
+}
+file {'path':
+    content => 'Holberton School',
+    path    => '/var/www/html/index.html',
 }
 service {'nginx':
     ensure  => running,
