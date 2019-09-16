cf3a1a763c81f780dc5d53a3aae6d5d10dea5970
JohnCook17 <655@holbertonschool.com>
2019-09-16 16:35:50 -0700

    pep8 and clean up

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,13 +4,24 @@ import sys
 import json
 import csv
 
-
-with open("USER_ID.csv", mode='w') as uid:
-    uid_writer = csv.writer(uid, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    uid_writer.writerow(["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"])
-    employee_id = sys.argv[1]
-    data = requests.get("https://jsonplaceholder.typicode.com/todos/", params={"userId": employee_id}).json()
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id)).json()
-    name = user.get("name")
-    for my_dict in data:
-        uid_writer.writerow(["{}".format(name), "{}".format(employee_id), "{}".format(my_dict.get("completed")), "{}".format(my_dict.get("title"))])
+if __name__ == "__main__":
+    with open("USER_ID.csv", mode='w') as uid:
+        uid_writer = csv.writer(uid,
+                                delimiter=',',
+                                quotechar='"',
+                                quoting=csv.QUOTE_MINIMAL)
+        uid_writer.writerow(["USER_ID",
+                             "USERNAME",
+                             "TASK_COMPLETED_STATUS",
+                             "TASK_TITLE"])
+        employee_id = sys.argv[1]
+        data = requests.get("https://jsonplaceholder.typicode.com/todos/",
+                            params={"userId": employee_id}).json()
+        user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(employee_id)).json()
+        name = user.get("name")
+        for my_dict in data:
+            uid_writer.writerow(["{}".format(name),
+                                 "{}".format(employee_id),
+                                 "{}".format(my_dict.get("completed")),
+                                 "{}".format(my_dict.get("title"))])
