706e373b0df9502056adafe2c1230256cc6d1de8
JohnCook17 <655@holbertonschool.com>
2019-09-16 17:09:21 -0700

    got quotes to look right and removed headers

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""gets data from api and saves to csv"""
+'''gets data from api and saves to csv'''
 import csv
 import json
 import requests
 import sys
 
 
-
-if __name__ == "__main__":
-    with open("USER_ID.csv", mode='w') as uid:
+if __name__ == '__main__':
+    with open('USER_ID.csv', mode='w') as uid:
         uid_writer = csv.writer(uid,
                                 delimiter=',',
                                 quotechar='"',
-                                quoting=csv.QUOTE_MINIMAL)
-        uid_writer.writerow(["USER_ID",
-                             "USERNAME",
-                             "TASK_COMPLETED_STATUS",
-                             "TASK_TITLE"])
+                                quoting=csv.QUOTE_ALL)
         employee_id = sys.argv[1]
-        data = requests.get("https://jsonplaceholder.typicode.com/todos/",
-                            params={"userId": employee_id}).json()
-        user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+        data = requests.get('https://jsonplaceholder.typicode.com/todos/',
+                            params={'userId': employee_id}).json()
+        user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                             .format(employee_id)).json()
-        name = user.get("name")
+        name = user.get('name')
         for my_dict in data:
-            uid_writer.writerow(["{}".format(name),
-                                 "{}".format(employee_id),
-                                 "{}".format(my_dict.get("completed")),
-                                 "{}".format(my_dict.get("title"))])
+            uid_writer.writerow([name,
+                                 employee_id,
+                                 my_dict.get('completed'),
+                                 my_dict.get('title')])
