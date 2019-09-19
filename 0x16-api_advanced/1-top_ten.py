258907400efa5378184bcf39a1f1d6b332f51838
JohnCook17 <655@holbertonschool.com>
2019-09-19 14:22:17 -0700

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """ gets the top 10 post of a subreddit"""
+    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+    data = requests.get("https://www.reddit.com/r/{}/hot/.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
+        print("None")
+    json_data = data.json().get("data").get("children", [])[0:10]
+    for post in json_data:
+        print(post.get("data").get("title"))
